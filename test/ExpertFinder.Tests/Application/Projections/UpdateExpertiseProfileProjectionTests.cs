5a47f3bdaf7bdfe720a3df5d3172e52eb9947fc1
Willem Meints <1550763+wmeints@users.noreply.github.com>
2023-09-24 07:38:12 +0200

    Add tests for user profile projection

---
+++ test/ExpertFinder.Tests/Application/Projections/UpdateExpertiseProfileProjectionTests.cs
@@ -0,0 +1,36 @@
+﻿using ExpertFinder.Application.Projections;
+using ExpertFinder.Domain.Aggregates.ArticleAggregate;
+using ExpertFinder.Domain.Aggregates.ArticleAggregate.Events;
+using ExpertFinder.Domain.Aggregates.UserAggregate;
+using ExpertFinder.Domain.Services;
+using ExpertFinder.Shared;
+using FakeItEasy;
+
+namespace ExpertFinder.Tests.Application.Projections;
+
+public class UpdateExpertiseProfileProjectionTests
+{
+    [Fact]
+    public async Task Handle_ShouldUpdateUserProfile()
+    {
+        var userId = Guid.NewGuid();
+        
+        var userRepository = A.Fake<IUserRepository>();
+        var articleRepository = A.Fake<IArticleRepository>();
+        var searchEngine = A.Fake<ISearchEngine>();
+        var unitOfWork = A.Fake<IUnitOfWork>();
+
+        A.CallTo(() => userRepository.GetUserByIdAsync(A<Guid>._)).Returns(new User(userId));
+        
+        var projection = new UpdateExpertiseProfileProjection(userRepository, articleRepository, searchEngine, unitOfWork);
+
+        var articlePublishedEvent = new ArticlePublishedEvent(
+            Guid.NewGuid(), "Test", "Test", 
+            userId, new float[1536]);
+        
+        await projection.Handle(articlePublishedEvent, CancellationToken.None);
+        
+        A.CallTo(() => unitOfWork.SaveChangesAsync(A<CancellationToken>._)).MustHaveHappened();
+        A.CallTo(() => searchEngine.IndexExpertProfileAsync(A<User>._)).MustHaveHappened();
+    }   
+}
